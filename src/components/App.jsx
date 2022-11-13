7eb489deb8b998ec408054f2ce0435cc6785261f
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2022-11-13 16:01:26 -0500

    replaced pathname with hash

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,9 @@ function App(){
     const location = useLocation();
     
     // Simplifies location pathname - pass to <Heading> and <Gallery>
-    // 1st remove everything before last slash, 2nd replace hyphens with spaces, 3rd remove special characters, 4th capitalize first letter of every word
-    const pageTitle = location.pathname.replace(/^\/[^\/\r\n]+(?=\/)/, "").replace(/-/g, ' ').replace(/[^a-zA-Z ]/g, "").replace(/(^\w{1})|(\s{1}\w{1})|(?:- |\d\. ).*/g, match => match.toUpperCase());
-    
+    // 1st replace hyphens with spaces, 2nd remove special characters, 3rd capitalize first letter of every word
+    const pageTitle = location.hash.replace(/-/g, ' ').replace(/[^a-zA-Z ]/g, "").replace(/(^\w{1})|(\s{1}\w{1})|(?:- |\d\. ).*/g, match => match.toUpperCase());
+
     // #region - generate number for button press
     const [imageNum, setImageNum] = useState(0);
 
