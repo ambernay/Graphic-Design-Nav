f0e39195c46e2c83f4e457b9e00bcf4e19692a56
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2023-01-25 11:24:27 -0500

    updated home link

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,7 +5,7 @@ function Heading(props){
   return  (
     <div className="heading-wrapper">
         <header className="heading title">{props.urlTitle}</header>
-        <a href="https://ambermoo.github.io/personal-website-repo/" className="heading home-link links">Home</a>
+        <a href="https://ambermoo.github.io/personal-website/" className="heading home-link links">Home</a>
     </div>
   )
 }
